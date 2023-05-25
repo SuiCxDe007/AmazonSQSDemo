4910c2e13fe715368ff763b47cb0647bb4520687
SuiCxDe <nitroxcyanide@gmail.com>
2023-05-25 22:56:58 +0530

    Establish connection to SQS,S3 & Setup listner

---
+++ src/main/java/com/example/amazonsqsdemo/SQSClientConfiguration.java
@@ -0,0 +1,40 @@
+package com.example.amazonsqsdemo;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SQSClientConfiguration {
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String awsAccessKey;
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String awsSecretKey;
+
+    @Bean
+    public AmazonSQS amazonSQSClient(){
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey,awsSecretKey);
+        return AmazonSQSClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.US_WEST_2)
+                .build();
+
+    }
+
+    @Bean
+    public AmazonS3 amazonS3Client(){
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey,awsSecretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.US_WEST_2)
+                .build();
+    }
+}
---
+++ src/main/java/com/example/amazonsqsdemo/SQSListener.java
@@ -0,0 +1,68 @@
+package com.example.amazonsqsdemo;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.List;
+
+@Service
+public class SQSListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SQSListener.class);
+
+    private final AmazonSQS sqs;
+    @Value("${cloud.aws.sqs.standard.queue.url}")
+    private String queueUrl;
+
+    private volatile boolean running = true;
+
+
+    @Autowired
+    public SQSListener(AmazonSQS sqs) {
+        this.sqs = sqs;
+    }
+
+    @PostConstruct
+    public void init(){
+        LOGGER.info("======================================================");
+        LOGGER.info("Starting SQS Listenere for Queue {}", queueUrl);
+        LOGGER.info("======================================================");
+        new Thread(()->{
+           while(running){
+               ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest()
+                       .withQueueUrl(queueUrl)
+                       .withWaitTimeSeconds(1)
+                       .withMessageAttributeNames("AuthToken");
+
+               List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
+               for(Message message : messages){
+                   LOGGER.info("Received Message with ID: {}",message.getMessageId());
+                   LOGGER.info("Attributes : {}",message.getMessageAttributes().get("AuthToken"));
+
+                   try{
+
+                   }catch (Exception e){
+                       throw new RuntimeException(e);
+                   }
+
+               }
+
+           }
+        }).start();
+    }
+
+    @PreDestroy
+    public void stop(){
+    LOGGER.info("Stopping SQS Listener -----------");
+    running=false;
+    }
+
+}
