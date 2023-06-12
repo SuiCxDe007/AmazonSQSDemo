6c47f6ba6101d98f785119219706e5971994dae3
SuiCxDe <nitroxcyanide@gmail.com>
2023-06-13 00:04:04 +0530

    added Swagger

--- src/main/java/com/example/amazonsqsdemo/SQSResource.java
+++ src/main/java/com/example/amazonsqsdemo/SQSResource.java
@@ -1,5 +1,6 @@
 package com.example.amazonsqsdemo;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +14,14 @@ public class SQSResource {
         this.sqsService = sqsService;
     }
 
+    @ApiOperation(value = "Send Message to Queue")
     @PostMapping("/sendMessage")
     public String sendMessage(@RequestHeader(name = "AuthToken") final String authToken,
                               @RequestBody final String message){
         return sqsService.sendMessage(authToken,message);
     }
 
+    @ApiOperation(value = "Get saved messages in S3")
     @GetMapping("/getMessage")
     public ResponseEntity<SQSMessage> getMessagebyId(@RequestParam("messageId") String messageId){
         return sqsService.getMessage(messageId);
